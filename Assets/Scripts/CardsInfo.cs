91daedcc2339f7f9b232741c2219f69648a6df6a
Darkchaton <ameliadulac12345@gmail.com>
2024-04-03 15:29:53 -0400

    ChangementDeCartesComplétés
    Maintenant quand on appuie sur le bouton générer, les cartes se génèrent toutes et apparaissent dans le mode Play, indépendamment des unes et des autres

--- Assets/Scripts/CardsInfo.cs
+++ Assets/Scripts/CardsInfo.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CardsInfo : MonoBehaviour
-{
-    public Text valeurText; // Référence au texte de la valeur de la carte
-    public Text couleurText; // Référence au texte de la couleur de la carte
+{ 
     public SpriteRenderer spriteRenderer; // Référence au SpriteRenderer pour l'image de la carte
 
     public enum ValeurCarte
@@ -48,13 +46,12 @@ public class CardsInfo : MonoBehaviour
     }
 
     public void AppliquerCarte(Carte carte)
-    {
-        // Assigne la valeur et la couleur de la carte au texte de la carte
-        valeurText.text = carte.valeur.ToString();
-        couleurText.text = carte.couleur.ToString();
-
+    {  
         // Charge l'image de la carte appropriée en fonction de sa valeur et sa couleur
         string cheminImage = carte.valeur.ToString() + "_" + carte.couleur.ToString();
-        spriteRenderer.sprite = Resources.Load<Sprite>(cheminImage);
+         
+        Debug.Log(cheminImage);
+
+        spriteRenderer.sprite = Resources.Load<Sprite>(cheminImage); 
     }
 }
--- Assets/Scripts/ScriptBoutonGenerer.cs
+++ Assets/Scripts/ScriptBoutonGenerer.cs
@@ -9,6 +9,11 @@ public class ScriptBoutonGenerer : MonoBehaviour
     public GameObject[] cartes; // Tableau d'objets carte à modifier
     public Carte[] toutesLesCartes; // Tableau de toutes les cartes disponibles
 
+    void Start()
+    {
+        GenererToutesLesCartes();
+    }
+
     public void GenererCartes()
     {
         // Pour chaque objet carte dans le tableau
@@ -24,4 +29,23 @@ public class ScriptBoutonGenerer : MonoBehaviour
             scriptCarte.AppliquerCarte(carteAleatoire);
         }
     }
+
+    void GenererToutesLesCartes()
+    {
+        toutesLesCartes = new Carte[52];
+
+        int index = 0;
+        foreach (CardsInfo.ValeurCarte valeur in System.Enum.GetValues(typeof(CardsInfo.ValeurCarte)))
+        {
+            foreach (CardsInfo.CouleurCarte couleur in System.Enum.GetValues(typeof(CardsInfo.CouleurCarte)))
+            {
+                // Création d'une nouvelle carte pour chaque combinaison de valeur et de couleur
+                toutesLesCartes[index] = new Carte(valeur, couleur);
+                Debug.Log(valeur + "," + couleur); 
+                index++;
+            }
+        }
+        Debug.Log(toutesLesCartes); 
+    }
+
 }
